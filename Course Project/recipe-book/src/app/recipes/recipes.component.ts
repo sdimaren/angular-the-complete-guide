c956c3bd46e47fcbf313046a71b377f84ee45452
Santiago Dimaren <sdimaren1@gmail.com>
2017-09-09 13:51:32 -0400

    outputting a list of recipes with ngfor

--- Course Project/recipe-book/src/app/recipes/recipes.component.ts
+++ Course Project/recipe-book/src/app/recipes/recipes.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from './recipe.model';
-
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-	recipes: Recipe[] = [
-		new Recipe('A Test Recipe', 'This is simply a test', 
-			'http://maxpixel.freegreatpicture.com/static/photo/1x/Recipe-Soup-Noodle-Curried-Spicy-Chicken-Khaosoi-2344152.jpg')
-	];
 
   constructor() { }
 
