bbeb0fe0d66cd0283d9a7fc7541b694aa0a71c93
Santiago Dimaren <sdimaren1@gmail.com>
2017-12-11 17:42:56 -0500

    reactive - setting and patching values

--- 08 - Forms/forms-reactive-start/src/app/app.component.ts
+++ 08 - Forms/forms-reactive-start/src/app/app.component.ts
@@ -21,13 +21,30 @@ export class AppComponent implements OnInit{
   		'gender': new FormControl('male'),
   		'hobbies': new FormArray([])
   	});
-  	this.signupForm.valueChanges.subscribe(
-  		(value) => console.log(value);
+  	// this.signupForm.valueChanges.subscribe(
+  	// 	(value) => console.log(value);
+  	// );
+  	this.signupForm.statusChanges.subscribe(
+  		(status) => console.log(status);
   	);
+  	this.signupForm.setValue({
+  		'userData': {
+  			'username': 'Max',
+  			'email': 'max@test.com'
+  		},
+  		'gender': 'male',
+  		'hobbies': []
+  	});
+  	this.signupForm.patchValue({
+  		'userData': {
+  			'username': 'Anna'
+  		}
+  	})
   }
 
   onSubmit() {
   	console.log(this.signupForm);
+  	this.signupForm.reset();
   }
 
   onAddHobby() {
