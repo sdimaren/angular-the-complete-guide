695c8cb4d15f9d9128d6f221e3bf554b66901c0f
Santiago Dimaren <sdimaren1@gmail.com>
2017-12-26 18:54:17 -0500

    adjusting request headers

--- 10 - Http/http-start/src/app/server.service.ts
+++ 10 - Http/http-start/src/app/server.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http'
+import { Headers, Http } from '@angular/http';
 
 @Injectable()
 export class ServerService {
 	constructor (private http: Http) {};
 	storeServers(servers: any[]) {
-		return this.http.post('https://ng-http-bdd46.firebaseio.com/data.json', servers);
+		const headers = new Headers({'Content-Type': 'application/json'});
+		return this.http.post('https://ng-http-bdd46.firebaseio.com/data.json',
+			servers, 
+			{headers: headers});
 	}
 }
