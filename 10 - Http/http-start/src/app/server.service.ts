1c6fa809830b61926742e60cb432cca2771c0e59
Santiago Dimaren <sdimaren1@gmail.com>
2017-12-26 19:00:54 -0500

    sending a put request

--- 10 - Http/http-start/src/app/server.service.ts
+++ 10 - Http/http-start/src/app/server.service.ts
@@ -6,9 +6,12 @@ export class ServerService {
 	constructor (private http: Http) {};
 	storeServers(servers: any[]) {
 		const headers = new Headers({'Content-Type': 'application/json'});
-		return this.http.post('https://ng-http-bdd46.firebaseio.com/data.json',
-			servers, 
-			{headers: headers});
+		// return this.http.post('https://ng-http-bdd46.firebaseio.com/data.json',
+		// 	servers, 
+		// 	{headers: headers});
+		return this.http.put('https://ng-http-bdd46.firebaseio.com/data.json',
+  	servers, 
+		{headers: headers});
 	}
 
 	getServers() {
