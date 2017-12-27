88f1b966427de2e5405708ba06f0f56989b7565e
Santiago Dimaren <sdimaren1@gmail.com>
2017-12-26 19:07:07 -0500

    transform responses with observable operators (map())

--- 10 - Http/http-start/src/app/server.service.ts
+++ 10 - Http/http-start/src/app/server.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, Response } from '@angular/http'
+import 'rxjs/Rx';
 
 @Injectable()
 export class ServerService {
@@ -15,6 +16,12 @@ export class ServerService {
 	}
 
 	getServers() {
-		return this.http.get('https://ng-http-bdd46.firebaseio.com/data.json');
+		return this.http.get('https://ng-http-bdd46.firebaseio.com/data.json')
+			.map(
+				(response: Response) => {
+					const data = response.json();
+					return data;
+				}
+			);
 	}
 }
