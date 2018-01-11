e6b135a0f34649ef3825d212ef16f49371c7ed34
Santiago Dimaren <sdimaren1@gmail.com>
2018-01-11 15:11:56 -0500

    adding lazy loading to the recipes module

--- Course Project/recipe-book/src/app/app.module.ts
+++ Course Project/recipe-book/src/app/app.module.ts
@@ -5,27 +5,26 @@ import { HttpModule } from '@angular/http';
 import { RecipeService } from './recipes/recipe.service';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    RecipesModule,
     ShoppingListModule,
     SharedModule,
     AuthModule
